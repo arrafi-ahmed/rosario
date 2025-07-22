afaf6db286b32cfb6366342dd54451c5567943f5
raf <rafi_lp@yahoo.com>
2025-07-23 01:23:10 +0700

    📦 Deploy commit on 2025-07-23 01:23:10

--- deploy-remote.sh
+++ deploy-remote.sh
@@ -88,8 +88,9 @@ sudo -u postgres psql -d "$DB_NAME" -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN
 sudo -u postgres psql -d "$DB_NAME" -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO $DB_USER;"
 sudo -u postgres psql -d "$DB_NAME" -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO $DB_USER;"
 
-# Add these new lines
-sudo -u postgres psql -d "$DB_NAME" -c "REASSIGN OWNED BY postgres TO $DB_USER;"
+# Replace the REASSIGN OWNED with these more specific commands
+sudo -u postgres psql -d "$DB_NAME" -c "SELECT 'ALTER TABLE \"' || schemaname || '\".\"' || tablename || '\" OWNER TO $DB_USER;' FROM pg_tables WHERE schemaname = 'public' \gexec"
+sudo -u postgres psql -d "$DB_NAME" -c "SELECT 'ALTER SEQUENCE \"' || sequence_schema || '\".\"' || sequence_name || '\" OWNER TO $DB_USER;' FROM information_schema.sequences WHERE sequence_schema = 'public' \gexec"
 sudo -u postgres psql -d "$DB_NAME" -c "ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES TO $DB_USER WITH GRANT OPTION;"
 
 echo "6.0 Cloning repo into $GLOBAL_CLONE_DIR..."
